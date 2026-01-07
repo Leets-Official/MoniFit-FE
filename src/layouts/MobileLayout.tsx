d1bfe33c21132f09ce5abadf7295b6f817f15e94
Issac <ivory.ma9ic@gmail.com>
2026-01-07 17:09:22 +0900

    design:layout 수정

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function MobileLayout() {
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-[#1f1f1f]">
+    <main className="flex h-full w-full items-center justify-center bg-[#1f1f1f]">
       <div className="flex h-203 w-93.75 flex-col border border-white px-4 py-6">
         <header className="flex h-8 w-full items-center justify-center bg-white">
           Header
