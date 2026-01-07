25d237a17a9674840637b0bddc727e5faad52505
Issac <ivory.ma9ic@gmail.com>
2026-01-08 00:37:25 +0900

    setting:alias 관련 설정 추가:

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "@/App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import MobileLayout from "./layouts/MobileLayout";
+import MobileLayout from "@/layouts/MobileLayout";
 
 import {
   TestForButtonPage,
