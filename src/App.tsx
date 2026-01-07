cdad01963b5a808a739f82ccd28b0384d3bd6099
Issac <ivory.ma9ic@gmail.com>
2026-01-08 00:37:25 +0900

    setting:alias 관련 설정 추가:

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import "@/App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MobileLayout from "@/layouts/MobileLayout";
-<<<<<<< HEAD
 
 import SignUpPage from "./pages/auth/SignUpPage";
-=======
->>>>>>> a80ba2e (setting:alias 관련 설정 추가:)
 
 function App() {
   return (
