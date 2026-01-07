ca551441c100296cdbcf163788d7b4c8d3e079be
Issac <ivory.ma9ic@gmail.com>
2026-01-07 17:05:17 +0900

    setting:outlet 및 기본 layout 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import MobileLayout from "./layouts/MobileLayout";
+
 function App() {
-  return <div className=""> test </div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MobileLayout />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
