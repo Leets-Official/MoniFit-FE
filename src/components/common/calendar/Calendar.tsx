5aaf9048c6afccfa4adb35424d962febf0a7d44b
Seol <pseol@gachon.ac.kr>
2026-01-14 16:54:31 +0900

    [#2]feat: WeekdayHeader 구현

--- src/components/common/calendar/Calendar.tsx
+++ src/components/common/calendar/Calendar.tsx
@@ -1,8 +1,10 @@
 import { DateGrid } from "./DateGrid";
+import { WeekdayHeader } from "./WeekdayHeader";
 
 export function Calendar() {
     return (
         <div>
+            <WeekdayHeader />
             <DateGrid />
         </div>
     );
---
+++ src/components/common/calendar/WeekdayHeader.tsx
@@ -0,0 +1,15 @@
+export function WeekdayHeader() {
+    const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
+    return (
+        <div className="grid grid-cols-7 w-[276.74px] h-[33.74px]">
+            {weekdays.map((day) => (
+                <div
+                    key={day}
+                    className="flex items-center justify-center text-[12.48px]"
+                >
+                    {day}
+                </div>                                                         
+            ))}
+        </div>
+    );
+}
