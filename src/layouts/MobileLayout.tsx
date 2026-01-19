df6059d3647ce0f6da575ec4cc8c667741962c82
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-20 02:00:12 +0900

    [#1]style:Button 공용 컴포넌트 스타일 수정

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function MobileLayout() {
   return (
     <main className="flex h-full w-full items-center justify-center bg-[#1f1f1f]">
-      <div className="flex h-203 w-93.75 flex-col border border-white px-4 py-6">
+      <div className="flex h-203 w-93.75 flex-col border-2 border-white">
         <header className="bg-point-mint flex h-8 w-full items-center justify-center">
           Header
         </header>
