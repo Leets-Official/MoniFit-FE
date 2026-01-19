bdc8396db30e659a16743eba0a9ad990fe050180
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-20 03:58:57 +0900

    [#4]feat:Input 공용 컴포넌트 구현

--- src/pages/test/TestForInputPage.tsx
+++ src/pages/test/TestForInputPage.tsx
@@ -5,8 +5,11 @@ export const TestForInputPage = () => {
     <>
       <main className="flex flex-col items-center justify-start w-full h-full gap-4 pt-10 bg-black opacity-70">
         <div className="flex flex-col items-center justify-center w-full gap-2">
-          <Input />
-          <Input width="medium" height="medium" />
+          <Input width="large" placeholder="금액을 입력하세요" />
+          <Input width="large" type="number" placeholder="금액을 입력하세요" />
+          <Input width="medium" placeholder="홍길동" />
+          <Input width="small" type="number" placeholder="홍길동" />
+          <Input width="small" placeholder="홍길동" />
         </div>
       </main>
     </>
