9bc08765cbed7399277deabdcd8b06d16b5d1449
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-20 05:16:16 +0900

    [#7]feat:CategoryButton 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "@/App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MobileLayout from "@/layouts/MobileLayout";
-import { TestForButtonPage, TestForInputPage } from "./pages/test";
+
 
 import {
   TestForButtonPage,
