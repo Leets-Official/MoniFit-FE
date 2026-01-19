c4dca61aae8f615bae2534aa0ceb6558678f5a63
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-19 18:26:14 +0900

    style:Color 및 Typo 시스템 적용

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -4,7 +4,7 @@ export default function MobileLayout() {
   return (
     <main className="flex h-full w-full items-center justify-center bg-[#1f1f1f]">
       <div className="flex h-203 w-93.75 flex-col border border-white px-4 py-6">
-        <header className="flex h-8 w-full items-center justify-center bg-white">
+        <header className="bg-point-mint text-h1 flex h-8 w-full items-center justify-center">
           Header
         </header>
         <Outlet />
