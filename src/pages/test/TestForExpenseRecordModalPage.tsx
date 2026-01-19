f8794e921831e07a41635d3193e312cf4eea5772
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-20 06:17:24 +0900

    [#9]feat:ExpenseRecordModal 컴포넌트 구현

---
+++ src/pages/test/TestForExpenseRecordModalPage.tsx
@@ -0,0 +1,17 @@
+import { Button, ExpenseRecordModal } from "@/components";
+import { useState } from "react";
+
+export const TestForExpenseRecordModalPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <main className="relative flex h-screen w-full flex-col items-center justify-start gap-4 bg-black pt-10 opacity-70">
+        <Button onClick={() => setShowModal(true)}>모달 열기</Button>
+        {showModal && (
+          <ExpenseRecordModal onClose={() => setShowModal(false)} />
+        )}
+      </main>
+    </>
+  );
+};
