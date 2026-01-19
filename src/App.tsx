6548378054f7c8bb1bcb2e96108143a36092f521
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-20 07:39:07 +0900

    [#11]style:구체 수정

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,10 @@ function App() {
               path="/test/liquid-sphere"
               element={<TestForLiquidSpherePage />}
             />
+            <Route
+              path="/test/liquid-sphere"
+              element={<TestForLiquidSpherePage />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
