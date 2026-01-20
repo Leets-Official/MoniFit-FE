8812c5a559e9adbe9200d5237e1b0e10c52a30d3
Seol <pseol@gachon.ac.kr>
2026-01-20 23:47:05 +0900

    [#2]test: CalendarTestPage 구현

---
+++ src/pages/test/CalendarTestPage.tsx
@@ -0,0 +1,9 @@
+import { Calendar } from "@/components/common/calendar/Calendar";
+
+export default function CalendarTestPage() {
+  return (
+    <div className="w-full h-full overflow-auto">
+      <Calendar />
+    </div>
+  );
+}
