e97cbff2e096ff50e9a2af7a43c04c71ad5094cf
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-21 05:31:30 +0900

    [#18]feat:ModalWrapper 컴포넌트 구현

---
+++ src/components/modal/ModalWrapper.tsx
@@ -0,0 +1,44 @@
+//! 26.01.12 작업 완료
+
+"use client";
+
+import { useEffect } from "react";
+
+import { lockBodyScroll, unlockBodyScroll } from "@/shared/lib/bodyScrollLock";
+import clsx from "clsx";
+
+type ModalWrapperProps = {
+  children: React.ReactNode;
+  className?: string;
+  onClose?: () => void;
+};
+
+export function ModalWrapper({
+  children,
+  className,
+  onClose,
+}: ModalWrapperProps) {
+  useEffect(() => {
+    lockBodyScroll();
+    return () => unlockBodyScroll();
+  }, []);
+
+  return (
+    <div
+      role="dialog"
+      aria-modal="true"
+      className="fixed inset-0 z-9999 flex items-center justify-center"
+    >
+      {/* dim overlay */}
+      <div className="absolute inset-0 bg-black/20" />
+      {/* modal content */}
+      <div className="relative z-10 opacity-100">{children}</div>
+      className=
+      {clsx("fixed inset-0 z-50 flex items-center justify-center", className)}
+      <div className="absolute inset-0 bg-black/40" onClick={onClose} />
+      <div className="relative z-10" onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
+}
