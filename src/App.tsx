353e2164ed839447f74c35308f0a20d4cfa996fe
Seol <pseol@gachon.ac.kr>
2026-01-21 01:37:26 +0900

    [#2]refactor: CalenderTestPage 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import "@/App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MobileLayout from "@/layouts/MobileLayout";
-import { Calendar } from "./components/common/calendar/Calendar";
+import CalendarTestPage from "@/pages/test/CalendarTestPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MobileLayout />}></Route>
-          <Route path="/calendar" element={<Calendar />}></Route>
+          <Route path="/" element={<MobileLayout />}>
+            <Route path="/test/calendar" element={<CalendarTestPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
