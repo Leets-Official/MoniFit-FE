e910fccb1c4739b17529789a2193256437ce01c4
Seol <pseol@gachon.ac.kr>
2026-01-21 11:43:25 +0900

    [#2]style: Calendar 정렬 변경

--- src/components/common/calendar/Calendar.tsx
+++ src/components/common/calendar/Calendar.tsx
@@ -4,13 +4,15 @@ import { CalendarHeader } from "./CalendarHeader";
 
 export function Calendar() {
     return (
-        <div>
-            <div>
-                <h2 className="font-semibold text-[20.29px] text-[#EAEAEA]">날짜를 선택해주세요.</h2>
+        <div className="flex flex-col items-center justify-center w-full py-10">
+            <div className="flex flex-col w-[276.2px]"> 
+                    <h2 className="font-semibold text-[20.29px] text-[#EAEAEA]">
+                        날짜를 선택해주세요.
+                    </h2>
+                <CalendarHeader />
+                <WeekdayHeader />
+                <DateGrid />
             </div>
-            <CalendarHeader />
-            <WeekdayHeader />
-            <DateGrid />
         </div>
     );
 }
