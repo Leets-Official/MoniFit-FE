7c794bae29136032d63a6a2c5271c8ab9a6e5ee1
Seol <pseol@gachon.ac.kr>
2026-01-23 17:56:49 +0900

    [#21]test: CategoryTestPage 추가

---
+++ src/pages/test/CategoryTestPage.tsx
@@ -0,0 +1,10 @@
+import CategoryItem from "@/components/category/CategoryItem";
+
+export default function CategoryTestPage() {
+  return (
+    <div className="w-full h-full overflow-auto">
+          <CategoryItem 
+          />
+        </div>
+  );
+}
