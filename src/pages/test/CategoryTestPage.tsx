c53eb508c109171529370026dbc6551bbd9946b6
Seol <pseol@gachon.ac.kr>
2026-01-24 13:41:07 +0900

    [#21]refactor: CategoryTestPage 컴포넌트 구조 변경

--- src/pages/test/CategoryTestPage.tsx
+++ src/pages/test/CategoryTestPage.tsx
@@ -3,8 +3,11 @@ import CategoryItem from "@/components/category/CategoryItem";
 export default function CategoryTestPage() {
   return (
     <div className="w-full h-full overflow-auto">
-          <CategoryItem 
-          />
+          <CategoryItem />
+          <CategoryItem />
+          <CategoryItem />
+          <CategoryItem />
+          <CategoryItem />
         </div>
   );
 }
