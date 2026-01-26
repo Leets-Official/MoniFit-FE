c3f05c8a694999c7ab204a7ea3389abfd445f145
Seol <pseol@gachon.ac.kr>
2026-01-26 22:19:17 +0900

    [#35]style: rect 요소 제거

--- src/components/common/icons/VerticalIndicator.tsx
+++ src/components/common/icons/VerticalIndicator.tsx
@@ -1,7 +1,6 @@
 const VerticalIndicator = () => {
     return (
-        <svg width="5" height="100" viewBox="0 0 5 45" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-            <rect width="5" height="100" rx="2.5" fill="#D1D0FF"/>
+        <svg width="5" height="45" viewBox="0 0 5 45" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
         </svg>
     );
 }   
