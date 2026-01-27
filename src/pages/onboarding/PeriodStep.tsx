275213b98c07490e8f2b294ee7a880f486beb564
Seol <pseol@gachon.ac.kr>
2026-01-27 18:01:41 +0900

    [#37]feat/style: 조건부 렌더링(최초 로그인/자동 로그인) 추가 및 스타일 수정

--- src/pages/onboarding/PeriodStep.tsx
+++ src/pages/onboarding/PeriodStep.tsx
@@ -1,12 +1,23 @@
 import { Button } from "@/components/common/Button";
 
-const PeriodStep = ({ onNext }: { onNext: () => void }) => {
+interface PeriodStepProps {
+  onNext: () => void;
+  isFirstLogin?: boolean; // 최초 로그인 여부 판단
+}
+
+const PeriodStep = ({ onNext, isFirstLogin = true }: PeriodStepProps) => {
   return (
     <div className="flex flex-col h-full">
-      <h1 className="text-2xl font-bold mt-10">
-        오늘부터 30일 동안<br />
-        예산을 관리해볼까요?
-      </h1>
+      <div className=" mb-8 items-center justify-center text-center">
+        {/* 최초 로그인일 때만 환영 문구 표시 */}
+        {isFirstLogin && (
+          <p className="text-[#EAEAEA] text-[24px] font-semibold mb-1"><span className="text-[#6E76AD]">모니핏</span>에 온 걸 환영해요!</p>
+        )}
+        <h1 className="text-[#EAEAEA] font-semibold leading-snug text-[20px]">
+          오늘부터 30일 동안<br />
+          예산을 관리해볼까요?
+        </h1>
+        </div>
       <div className="flex-1 flex items-center justify-center text-gray-500">
         [달력 컴포넌트 배치 예정]
       </div>
