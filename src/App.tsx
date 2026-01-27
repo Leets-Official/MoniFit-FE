f755465b06e768f93171a23568cfae2cc718bc2c
Saelyeam <eileen4505@gachon.ac.kr>
2026-01-28 01:46:02 +0900

    chore: 마이페이지 라우팅 연결

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MobileLayout from "@/layouts/MobileLayout";
 import CalendarTestPage from "@/pages/test/CalendarTestPage";
-import CategoryTestPage from "@/pages/test/CategoryTestPage";
 import DonutChartTestPage from "@/pages/DonutChartTestPage";
 
 import {
@@ -18,39 +17,19 @@ import {
 } from "./pages/test";
 
 import { MainPage } from "./pages";
+import { MyPageMainPage, MyPageNoticePage, MyPageProfilePage } from "./pages/mypage";
 
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MobileLayout />}>
-            <Route path="/" element={<MainPage />} />
-            {/* 테스트 페이지용 라우팅 */}
-            <Route path="/test/calendar" element={<CalendarTestPage />} />
-            <Route path="/test/category" element={<CategoryTestPage />} />
-            <Route path="/test/button" element={<TestForButtonPage />} />
-            <Route path="/test/input" element={<TestForInputPage />} />
-            <Route
-              path="/test/category-button"
-              element={<TestForCategoryButtonPage />}
-            />
-            <Route
-              path="/test/modal/expense-record"
-              element={<TestForExpenseRecordModalPage />}
-            />
-            <Route
-              path="/test/sphere/liquid-sphere"
-              element={<TestForLiquidSpherePage />}
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MobileLayout />}>
           <Route path="/" element={<MainPage />} />
+
+          {/* MyPage */}
+          <Route path="/mypage" element={<MyPageMainPage />} />
+          <Route path="/mypage/profile" element={<MyPageProfilePage />} />
+          <Route path="/mypage/notice" element={<MyPageNoticePage />} />
 
           {/* 테스트 페이지용 라우팅 */}
           <Route path="/test/calendar" element={<CalendarTestPage />} />
@@ -58,29 +37,12 @@ export default function App() {
 
           <Route path="/test/button" element={<TestForButtonPage />} />
           <Route path="/test/input" element={<TestForInputPage />} />
-          <Route
-            path="/test/category-button"
-            element={<TestForCategoryButtonPage />}
-          />
-          <Route
-            path="/test/modal/expense-record"
-            element={<TestForExpenseRecordModalPage />}
-          />
-          <Route
-            path="/test/sphere/liquid-sphere"
-            element={<TestForLiquidSpherePage />}
-          />
+          <Route path="/test/category-button" element={<TestForCategoryButtonPage />} />
+          <Route path="/test/modal/expense-record" element={<TestForExpenseRecordModalPage />} />
+          <Route path="/test/sphere/liquid-sphere" element={<TestForLiquidSpherePage />} />
           <Route path="/test/side-sheet" element={<TestForSideSheetPage />} />
-
-          <Route
-            path="/test/budget-progress-card"
-            element={<TestForBudgetProgressCardPage />}
-          />
-          
-          <Route
-            path="/test/common-card"
-            element={<TestForCommonCardPage />}
-          />
+          <Route path="/test/budget-progress-card" element={<TestForBudgetProgressCardPage />} />
+          <Route path="/test/common-card" element={<TestForCommonCardPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
