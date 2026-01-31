f0c78d7fb6fef7c0bb28fef481de741698f4c7ef
Saelyeam <eileen4505@gachon.ac.kr>
2026-02-01 00:36:26 +0900

    feat: MainPage에 ReportPage(팝업)연결

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,12 +4,13 @@ import { ModalWrapper } from "@/components/modal/ModalWrapper";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
 import CalendarPage from "./CalendarPage";
+import ReportPage from "./ReportPage";
 
-// TODO : MOCK_DATA
 const TOTAL_AMOUNT = 1000000;
 
 export const MainPage = () => {
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [isReportOpen, setIsReportOpen] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [percent, setPercent] = useState(100);
   const [spent, setSpent] = useState(0);
@@ -18,12 +19,8 @@ export const MainPage = () => {
   const mixColor = (from: string, to: string, t: number) => {
     const f = parseInt(from.replace("#", ""), 16);
     const tt = parseInt(to.replace("#", ""), 16);
-    const r = Math.round(
-      ((f >> 16) & 255) + (((tt >> 16) & 255) - ((f >> 16) & 255)) * t,
-    );
-    const g = Math.round(
-      ((f >> 8) & 255) + (((tt >> 8) & 255) - ((f >> 8) & 255)) * t,
-    );
+    const r = Math.round(((f >> 16) & 255) + (((tt >> 16) & 255) - ((f >> 16) & 255)) * t);
+    const g = Math.round(((f >> 8) & 255) + (((tt >> 8) & 255) - ((f >> 8) & 255)) * t);
     const b = Math.round((f & 255) + ((tt & 255) - (f & 255)) * t);
     return `rgb(${r} ${g} ${b})`;
   };
@@ -34,10 +31,7 @@ export const MainPage = () => {
     setSpent((prev) => prev + expense);
 
     const nextSpent = spent + expense;
-    const nextPercent = Math.min(
-      100,
-      ((TOTAL_AMOUNT - nextSpent) / TOTAL_AMOUNT) * 100,
-    );
+    const nextPercent = Math.min(100, ((TOTAL_AMOUNT - nextSpent) / TOTAL_AMOUNT) * 100);
 
     setPercent(nextPercent);
     setShowModal(false);
@@ -52,6 +46,7 @@ export const MainPage = () => {
           <span>2026.01.31</span>
         </div>
       </section>
+
       <section className="relative">
         <div
           className="relative h-90 w-90"
@@ -65,21 +60,19 @@ export const MainPage = () => {
             <LiquidSphere percent={percent} />
           </Canvas>
         </div>
+
         <div className="absolute -bottom-30 left-1/2 flex -translate-x-1/2 flex-col items-center">
           <span className="text-body2 text-[#8A8A8A]">남은 금액</span>
           <span className="text-h1 text-gray-0 flex max-w-70 items-center gap-2 overflow-x-scroll">
             <span>₩</span>
             <span>{(TOTAL_AMOUNT - spent).toLocaleString()}</span>
           </span>
-          <Button
-            width="md"
-            className="mt-3.5"
-            onClick={() => setShowModal(true)}
-          >
+          <Button width="md" className="mt-3.5" onClick={() => setShowModal(true)}>
             지출 입력하기
           </Button>
         </div>
       </section>
+
       <section className="absolute bottom-20 flex w-full items-center justify-center gap-3">
         <Button
           width={"sm"}
@@ -87,33 +80,46 @@ export const MainPage = () => {
           bgColor={"none"}
           className="flex gap-2"
           fontColor={"white"}
-          onClick={() => setIsCalendarOpen(true)}
+          onClick={() => {
+            setIsCalendarOpen(true);
+            setIsReportOpen(false);
+          }}
         >
           <CalendarIcon />
           <span>달력</span>
         </Button>
+
         <Button
           width={"md"}
           borderColor={"outline"}
           bgColor={"none"}
           className="flex gap-2"
           fontColor={"white"}
+          onClick={() => {
+            setIsReportOpen(true);
+            setIsCalendarOpen(false);
+          }}
         >
           <ReportIcon />
           <span>리포트</span>
         </Button>
       </section>
+
       {isCalendarOpen && (
         <div className="fixed inset-0 z-50 bg-transparent">
           <CalendarPage onClose={() => setIsCalendarOpen(false)} />
         </div>
       )}
+
+      {isReportOpen && (
+        <div className="fixed inset-0 z-50 bg-transparent">
+          <ReportPage onClose={() => setIsReportOpen(false)} />
+        </div>
+      )}
+
       {showModal && (
         <ModalWrapper onClose={() => setShowModal(false)}>
-          <ExpenseRecordModal
-            onClose={() => setShowModal(false)}
-            onSave={handleSaveExpense}
-          />
+          <ExpenseRecordModal onClose={() => setShowModal(false)} onSave={handleSaveExpense} />
         </ModalWrapper>
       )}
     </main>
