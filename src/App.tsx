ff07be5e52ed6a3abba08c47d630bb2ed1c1137a
Seol <pseol@gachon.ac.kr>
2026-01-31 22:41:32 +0900

    [#44]feat: LoginPage 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import CalendarTestPage from "@/pages/test/CalendarTestPage";
 import DonutChartTestPage from "@/pages/DonutChartTestPage";
 
 import SplashPage from "./pages/auth/SplashPage";
+import LoginPage from "./pages/auth/LoginPage";
 import CalendarPage from "./pages/CalendarPage";
 import { MainPage } from "./pages";
 import { MyPageMainPage, MyPageNoticePage, MyPageProfilePage } from "./pages/mypage";
@@ -28,6 +29,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MobileLayout />}>
           <Route index element={<SplashPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/onboarding/budget-setting" element={<BudgetSettingPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
