084632a39464f1d202d496fd53edcf86cad3c56f
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-02-04 08:16:42 +0900

    [#42]chore:패키지 재설치

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api/v1', 
+  baseURL: "/api/v1",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // 모든 요청(Request) 시 자동으로 토큰을 헤더에 넣어주는 인터셉터
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('accessToken');
+  const token = localStorage.getItem("accessToken");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
