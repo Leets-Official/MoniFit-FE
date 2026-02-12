47c428f528c05f4868747c58ba1059f7dbb02be4
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-02-13 08:42:56 +0900

    style: html, body 태그에 최대 너비/높이 제약 적용 및 MobileLayout 단순화

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -2,10 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function MobileLayout() {
   return (
-    <div className="flex min-h-dvh justify-center bg-[#1f1f1f]">
-      <main className="flex h-dvh max-h-[812px] w-[375px] flex-col overflow-hidden">
-        <Outlet />
-      </main>
-    </div>
+    <main className="flex h-full max-h-[812px] w-full max-w-[376px] flex-col overflow-hidden">
+      <Outlet />
+    </main>
   );
 }
